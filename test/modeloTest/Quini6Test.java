02f03f30a0d4a92745eaeb56e79a179b6ff747d3
tomasaccini <tomasaccini@gmail.com>
2017-11-10 18:24:15 -0300

    Agregado de tests a la clase Quini6Test para asegurar su correcto funcionamiento

--- test/modeloTest/Quini6Test.java
+++ test/modeloTest/Quini6Test.java
@@ -15,19 +15,18 @@ public class Quini6Test {
     }
 
     @Test
-    public void testAlPagarUnJugadorPorPrimeraVezLeAumentaElSaldoEn50000() {
+    public void testAlDarPremioAUnJugadorPorPrimeraVezLeAumentaElSaldoEn50000() {
 
         Quini6 unQuini = new Quini6();
         Jugador unJugador = new Jugador();
         int saldo_inicial = unJugador.verSaldo();
         unQuini.darPremio(unJugador);
 
-
         Assert.assertEquals(unJugador.verSaldo(), saldo_inicial + 50000);
     }
 
     @Test
-    public void testAlPagarUnJugadorPorSegundaVezLeAumentaElSaldoEn30000() {
+    public void testAlDarPremioAUnJugadorPorSegundaVezLeAumentaElSaldoEn30000() {
 
         Quini6 unQuini = new Quini6();
         Jugador unJugador = new Jugador();
@@ -39,7 +38,7 @@ public class Quini6Test {
     }
 
     @Test
-    public void testAlPagarUnJugadorPorTerceraVezLeAumentaElSaldoEn30000() {
+    public void testAlDarPremioAUnJugadorPorTerceraVezLeAumentaElSaldoEn0() {
 
         Quini6 unQuini = new Quini6();
         Jugador unJugador = new Jugador();
@@ -47,7 +46,48 @@ public class Quini6Test {
         unQuini.darPremio(unJugador);
         int saldo_final = unJugador.verSaldo();
         unQuini.darPremio(unJugador);
+
         Assert.assertEquals(unJugador.verSaldo(), saldo_final);
+    }
+
+    @Test
+    public void testAlDarPremioAUnJugador1000VecesLeAumentaElSaldoEn80000() {
+
+        Quini6 unQuini = new Quini6();
+        Jugador unJugador = new Jugador();
+        int saldo_inicial = unJugador.verSaldo();
+        for (int i = 0; i < 1000; i++) {
+            unQuini.darPremio(unJugador);
+        }
+        Assert.assertEquals(unJugador.verSaldo(), saldo_inicial + 80000);
+    }
+
+    @Test
+    public void testAlRecibirDosPremiosUnJugadorConUnaEntregaEnElMedioAOtroJugadorRecibeBienElPremio() {
+
+        Quini6 unQuini = new Quini6();
+        Jugador JugadorA = new Jugador();
+        Jugador JugadorB = new Jugador();
+
+        unQuini.darPremio(JugadorA);
+        unQuini.darPremio(JugadorB);
+        unQuini.darPremio(JugadorA);
+
+        Assert.assertEquals(JugadorA.verSaldo(), 100000 + 50000 + 30000);
+    }
+
+    @Test
+    public void testAlRecibirUnPremiosUnJugadorEntreDosEntregasAOtroJugadorRecibeBienElPremio() {
+
+        Quini6 unQuini = new Quini6();
+        Jugador JugadorA = new Jugador();
+        Jugador JugadorB = new Jugador();
+
+        unQuini.darPremio(JugadorA);
+        unQuini.darPremio(JugadorB);
+        unQuini.darPremio(JugadorA);
+
+        Assert.assertEquals(JugadorB.verSaldo(), 100000 + 50000);
     }
 
 
