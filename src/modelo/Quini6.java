ad4a9524ef7de036112a5177b0097fcb6094135c
tomasaccini <tomasaccini@gmail.com>
2017-11-10 13:07:42 -0300

    Agregué las clases Quini6 y Quini6Test, probando su correcta instanciación. PP: Jonathan

---
+++ src/modelo/Quini6.java
@@ -0,0 +1,7 @@
+package modelo;
+
+public class Quini6 {
+
+
+
+}
---
+++ test/modelo/Quini6Test.java
@@ -0,0 +1,17 @@
+package test.modelo;
+import modelo.Quini6;
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class Quini6Test {
+    @Test
+    public void testAlCrearQuini6NoDevuelveNull() {
+
+        Quini6 unQuini = new Quini6();
+
+        Assert.assertNotNull(unQuini);
+    }
+
+}
+    
