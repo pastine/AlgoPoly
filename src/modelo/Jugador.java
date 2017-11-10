4f75fd6569a7a0274873016a483ad3579a6305fd
pastine <casimiropastine@gmail.com>
2017-11-10 13:16:03 -0300

    agrego clase jugador y sus tests unitarios

--- src/modelo/Jugador.java
+++ src/modelo/Jugador.java
@@ -1,5 +1,22 @@
 package modelo;
 
 public class Jugador {
-
+	private int saldo;
+	
+	
+	public Jugador() {
+		this.saldo=100000;
+	}
+	
+	public int verSaldo() {
+		return this.saldo;
+	}
+	
+	public void recibirDinero(int dinero) {
+		this.saldo += dinero;
+	}
+	
+	public void quitarDinero(int cantidad) {
+		this.saldo -= cantidad;
+	}
 }
--- test/modelo/JugadorTest.java
+++ test/modelo/JugadorTest.java
@@ -1,5 +1,32 @@
-package test.modelo;
+package modelo;
 
-public class JugadorTest {
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+class JugadorTest {
+
+	@Test
+	void sumarPlataAUnJugadorLeSumaCorrectamenteTest() {
+		Jugador jugador = new Jugador();
+		jugador.recibirDinero(500);
+		
+		Assert.assertEquals(jugador.verSaldo(),100500);
+	}
+	
+	@Test
+	void elJugadorSeinicializaConSaldo100000Test() {
+		Jugador jugador = new Jugador();
+		
+		Assert.assertEquals(jugador.verSaldo(), 100000);
+	}
+	
+	@Test
+	void elSaldoDelJugadorSeReduceCorrectamenteTest() {
+		Jugador jugador = new Jugador();
+		
+		jugador.quitarDinero(50000);
+		
+		Assert.assertEquals(jugador.verSaldo(),50000);
+	}
 
 }
