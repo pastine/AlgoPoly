3c44458b82aab273c92218371a7afbc891acf738
pastine <casimiropastine@gmail.com>
2017-11-17 14:02:30 -0300

    Borro codigo comentado e import innecesario en quini6

--- src/modelo/Quini6.java
+++ src/modelo/Quini6.java
@@ -1,26 +1,7 @@
 package modelo;
 
-import java.util.HashMap;
-
 public class Quini6 {
-/*
-    private HashMap<Jugador, Integer> visitas;
-    private HashMap<Integer, Integer> premio;
 
-
-    public Quini6() {
-        this.visitas = new HashMap<Jugador, Integer>();
-        this.premio = new HashMap<Integer, Integer>();
-        premio.put(1,50000);
-        premio.put(2,30000);
-    }
-
-    public void darPremio(Jugador unJugador) {
-        int numeroVisitas = visitas.getOrDefault(unJugador, 0) + 1;
-        visitas.put(unJugador, numeroVisitas);
-        unJugador.recibirDinero(premio.getOrDefault(numeroVisitas,0));
-    }
-*/
 	public void darPremio(Jugador unJugador){
 		unJugador.recibirPremioQuini();
 	}
