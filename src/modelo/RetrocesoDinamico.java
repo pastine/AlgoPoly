0e76448261929d703bae7c40ec02c667862ab6a4
Jonathan <jonathanm_96@hotmail.com>
2017-11-18 17:58:42 -0300

    reordeno el retroceso dinamico

--- src/modelo/RetrocesoDinamico.java
+++ src/modelo/RetrocesoDinamico.java
@@ -5,24 +5,25 @@ import modelo.jugador.Jugador;
 public class RetrocesoDinamico extends MovimientoDinamico{
 	
 	public RetrocesoDinamico(){
-		primerValorMin = 7;
-		segundoValorMin = 11;
-		tercerValorMin = 2 ;
+		primerValorMin = 2;
+		segundoValorMin = 7;
+		tercerValorMin = 11 ;
 		
-		primerValorMax =10;
-		segundoValorMax = 12;
-		tercerValorMax= 6;
+		primerValorMax = 6;
+		segundoValorMax = 10;
+		tercerValorMax= 12;
 	}
-
-	@Override
-	protected void segundoCaso(Jugador unJugador, int numeroObtenido) {
-		unJugador.cambiarDireccion();
-		super.resultadoMenosDos(unJugador,numeroObtenido);
-		unJugador.cambiarDireccion();
-	}
-
+	
 	@Override
 	protected void primerCaso(Jugador unJugador, int numeroObtenido) {
+		unJugador.cambiarDireccion();
+		super.resultadoPorCantidadDePropiedades(unJugador, numeroObtenido);
+		unJugador.cambiarDireccion();
+	}
+
+	
+	@Override
+	protected void segundoCaso(Jugador unJugador, int numeroObtenido) {
 		unJugador.cambiarDireccion();
 		super.restoDeDivision(unJugador, numeroObtenido);
 		unJugador.cambiarDireccion();
@@ -31,7 +32,7 @@ public class RetrocesoDinamico extends MovimientoDinamico{
 	@Override
 	protected void tercerCaso(Jugador unJugador, int numeroObtenido) {
 		unJugador.cambiarDireccion();
-		super.resultadoPorCantidadDePropiedades(unJugador, numeroObtenido);
+		super.resultadoMenosDos(unJugador,numeroObtenido);
 		unJugador.cambiarDireccion();
 	}
 }
