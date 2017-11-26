9a198ed12548781bc930ae6cb10f32bd69b5ee2e
tomasaccini <tomasaccini@gmail.com>
2017-11-26 13:02:51 -0300

    Agregada la logica de habilitar/deshabilitar botones

--- src/vista/eventos/BotonFinalizarTurnoEventHandler.java
+++ src/vista/eventos/BotonFinalizarTurnoEventHandler.java
@@ -2,19 +2,24 @@ package vista.eventos;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.control.Button;
 import modelo.AlgoPoly;
+import vista.ContenedorPrincipal;
 
 public class BotonFinalizarTurnoEventHandler implements EventHandler<ActionEvent>{
 
+	ContenedorPrincipal contenedor;
 	AlgoPoly algoPoly;
 	
-	public BotonFinalizarTurnoEventHandler (AlgoPoly algoPoly) {
+	public BotonFinalizarTurnoEventHandler (ContenedorPrincipal contenedor,AlgoPoly algoPoly) {
+		this.contenedor = contenedor;
 		this.algoPoly = algoPoly;
 	}
 	
 	@Override
     public void handle(ActionEvent actionEvent) {
 		algoPoly.finalizarTurno();
+		contenedor.deshabilitarBotonesAlFinalizarTurno();
 	}
 	
 }
--- src/vista/eventos/BotonMoverEventHandler.java
+++ src/vista/eventos/BotonMoverEventHandler.java
@@ -2,26 +2,32 @@ package vista.eventos;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.control.Button;
 import modelo.AlgoPoly;
 import modelo.jugador.Jugador;
+import vista.ContenedorPrincipal;
 import vista.VistaJugador;
 
+
+
 public class BotonMoverEventHandler implements EventHandler<ActionEvent> {
-	
+	ContenedorPrincipal contenedor;
 	AlgoPoly algoPoly;
 	VistaJugador vistaJugador;
 
-	public BotonMoverEventHandler (AlgoPoly algoPoly, VistaJugador vistaJugador) {
+	public BotonMoverEventHandler (ContenedorPrincipal contenedor, AlgoPoly algoPoly, VistaJugador vistaJugador) {
+		this.contenedor = contenedor;
 		this.algoPoly = algoPoly;
 		this.vistaJugador = vistaJugador;
 	}
 	
     @Override
     public void handle(ActionEvent actionEvent) {
+		contenedor.deshabilitarBotonesTrasMover();
     	Jugador jugadorActual = algoPoly.devolverJugadorActual();
     	int pasosTotal = algoPoly.lanzarDadosYMover();
         vistaJugador.update(pasosTotal, jugadorActual);
         System.out.println("DEBUG: El jugador " + jugadorActual.toString() + " avanzo "+ pasosTotal + " pasos y tiene $" + jugadorActual.obtenerSaldo());
-    }
+	}
     
 }
